d1ea14eed583c5d5c88d40b033f8d53cc7389f39
jaideep <jaideepphalswal12@gmail.com>
2021-01-30 18:43:02 +0530

    complete site

--- src/components/googleDrive/AddFolderButton.js
+++ src/components/googleDrive/AddFolderButton.js
@@ -2,10 +2,14 @@ import React, { useState } from 'react'
 import { Button, Modal, Form } from 'react-bootstrap'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faFolderPlus } from '@fortawesome/free-solid-svg-icons'
+import { database } from '../../firebase'
+import { useAuth } from '../../contexts/AuthContext'
+import { ROOT_FOLDER } from '../../hooks/useFolder'
 
-export default function AddFolderButton() {
+export default function AddFolderButton({ currentFolder }) {
     const [open, setOpen] = useState(false);
     const [name, setName] = useState('');
+    const { currentUser } = useAuth();
 
     function openModal() {
         setOpen(true);
@@ -13,19 +17,45 @@ export default function AddFolderButton() {
     function closeModal() {
         setOpen(false);
     }
+    function handleSubmit(e) {
+        e.preventDefault()
+
+        if (currentFolder == null) return;
+
+        const path = [...currentFolder.path]
+        if (currentFolder !== ROOT_FOLDER) {
+            path.push({ name: currentFolder.name, id: currentFolder.id })
+        }
+
+        //Create a folder in the database
+        database.folders.add({
+            name: name,
+            parentId: currentFolder.id,
+            userId: currentUser.uid,
+            createdAt: database.getCurrentTimestamp(),
+            path: path,
+        })
+        setName("")
+        closeModal()
+    }
     return (
         <>
             <Button onClick={openModal} variant="outline-success" size='sm '>
                 <FontAwesomeIcon icon={faFolderPlus} />
             </Button>
             <Modal show={open} onHide={closeModal} >
-                <Form>
+                <Form onSubmit={handleSubmit}>
                     <Modal.Body>
                         <Form.Label>Folder Name</Form.Label>
-                        <Form.Control type="text" require value={name} onChange={e => setName(e.target.value)} />
+                        <Form.Control type="text" required value={name} onChange={e => setName(e.target.value)} />
                     </Modal.Body>
                     <Modal.Footer>
-                        <Button variant="secondary" onClick={closeModal} />
+                        <Button variant="secondary" onClick={closeModal}>
+                            Close
+                        </Button>
+                        <Button variant="success" type="submit" >
+                            Add Folder
+                        </Button>
                     </Modal.Footer>
                 </Form>
             </Modal>
--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,7 @@
 import firebase from 'firebase/app'
 import 'firebase/auth'
-
+import 'firebase/firestore'
+import 'firebase/storage'
 const app = firebase.initializeApp({
     apiKey: "AIzaSyCz1z4se68ujKlPw3DgpmDPMp3GTsQM0kY",
     authDomain: "drive-clone-56f21.firebaseapp.com",
@@ -11,5 +12,16 @@ const app = firebase.initializeApp({
     measurementId: "G-6HXNSEV3ZF"
 })
 
+const firestore = app.firestore()
+export const database = {
+    folders: firestore.collection('folders'),
+    files: firestore.collection('files'),
+    formatDoc: doc => {
+        return { id: doc.id, ...doc.data() }
+    },
+    getCurrentTimestamp: firebase.firestore.FieldValue.serverTimestamp
+}
+
+export const storage = app.storage()
 export const auth = app.auth();
 export default app;
